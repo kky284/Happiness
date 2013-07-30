117a6f7d651bc902318ad959b2a919eed298bf38
kky284 <kky284@gmail.com>
2013-07-30 17:32:14 +0900

    Initial Commit

---
+++ Happiness/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Happiness
+//
+//  Created by Ki-Yong Kim on 7/30/13.
+//  Copyright (c) 2013 Ki-Yong Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
